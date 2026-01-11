298ca66a7770c6b141dabd9d58ef779a51f2bd4c
kazmrks <kaz.mrks@gmail.com>
2026-01-11 11:22:44 +0000

    ABC430 a-b AC, c WA

---
+++ ABC430/b/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func init() {
+	sc.Buffer([]byte{}, math.MaxInt64)
+}
+
+func nextLine() string {
+	sc.Scan()
+	return sc.Text()
+}
+
+func scanInt() int {
+	i, err := strconv.Atoi(nextLine())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func lineToInts() []int {
+	slice := []int{}
+	text := strings.Split(nextLine(), " ")
+
+	for _, t := range text {
+		v, err := strconv.Atoi(t)
+		if err != nil {
+			panic(err)
+		}
+		slice = append(slice, v)
+	}
+	return slice
+}
+
+func printInts(s []int) {
+	p := []string{}
+	for _, v := range s {
+		p = append(p, strconv.Itoa(v))
+	}
+	fmt.Println(strings.Join(p, " "))
+}
+
+func printStrings(s []string) {
+	fmt.Println(strings.Join(s, " "))
+}
+
+func contains(elements []int, v int) bool {
+	for _, e := range elements {
+		if e == v {
+			return true
+		}
+	}
+	return false
+}
+
+// 順列全探索
+func nextPermutation(x sort.Interface) bool {
+	n := x.Len() - 1
+	if n < 1 {
+		return false
+	}
+	j := n - 1
+	for ; !x.Less(j, j+1); j-- {
+		if j == 0 {
+			return false
+		}
+	}
+	l := n
+	for !x.Less(j, l) {
+		l--
+	}
+	x.Swap(j, l)
+	for k, l := j+1, n; k < l; {
+		x.Swap(k, l)
+		k++
+		l--
+	}
+	return true
+}
+
+func main() {
+	line := lineToInts()
+	N, M := line[0], line[1]
+
+	S := []string{}
+	for i := 0; i < N; i++ {
+		S = append(S, nextLine())
+	}
+
+	grid := map[string]int{}
+	for i := 0; i < N-M+1; i++ {
+		for j := 0; j < N-M+1; j++ {
+			text := ""
+			for ii := i; ii < i+M; ii++ {
+				for jj := j; jj < j+M; jj++ {
+					text += string(S[ii][jj])
+				}
+			}
+			grid[text]++
+		}
+	}
+	fmt.Println(len(grid))
+}
